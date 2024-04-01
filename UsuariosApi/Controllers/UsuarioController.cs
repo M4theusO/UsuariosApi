9c3558be24b67ff845fb69a0d17334a1a7237192
Matheus de Oliveira <94876894+M4theusO@users.noreply.github.com>
2024-04-01 17:43:43 -0300

    Initial commit

---
+++ UsuariosApi/Controllers/UsuarioController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.Data.Dtos;
+using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
+
+namespace UsuariosApi.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CadastraUsuario(CreateUsuarioDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
